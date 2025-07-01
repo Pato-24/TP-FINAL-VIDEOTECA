f7470c691e708919222292e32990fe8afa74612a
Pato-24 <parguello24@gmail.com>
2025-06-30 23:10:57 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Integrantes from "./pages/Integrantes";
+import Usuarios from "./pages/Usuarios";
 
 
 function App() {

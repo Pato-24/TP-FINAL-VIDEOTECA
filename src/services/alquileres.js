f13e4e1611d6d7cba4c28d021927d7795f8edee0
Pato-24 <parguello24@gmail.com>
2025-07-01 18:41:49 -0300

    correccion de errores en el formulario de alquileres

--- src/services/alquileres.js
+++ src/services/alquileres.js
@@ -4,13 +4,13 @@ import axios from "axios";
 const API_URL = "http://localhost:3001/alquileres";
 
 // Trae todos los alquileres (GET)
-export const getAlquileres = () => axios.get(API);
+export const getAlquileres = () => axios.get(API_URL);
 
 // Agrega un nuevo alquiler (POST)
 export const addAlquiler = (alquiler) => axios.post(API_URL, alquiler);
 
 // Edita un alquiler existente por ID (PUT)
-export const editAlquiler = (id, data) => axios.put(`${API}/${id}`, data);
+export const editAlquiler = (id, data) => axios.put(`${API_URL}/${id}`, data);
 
 // Elimina un alquiler por ID (DELETE)
-export const deleteAlquiler = (id) => axios.delete(`${API}/${id}`);
+export const deleteAlquiler = (id) => axios.delete(`${API_URL}/${id}`);
